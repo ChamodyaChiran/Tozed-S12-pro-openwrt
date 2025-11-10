9446caea277ebc94a4376066477c8314331e2d04
Chamodya Chiran <62944143+ChamodyaChiran@users.noreply.github.com>
2025-11-10 23:28:38 +0530

    Update install_passwall.sh

--- install_passwall.sh
+++ install_passwall.sh
@@ -3,15 +3,15 @@ wget -O passwall.pub https://master.dl.sourceforge.net/project/openwrt-passwall-
 opkg-key add passwall.pub
 opkg update
 opkg remove dnsmasq
-opkg install  ipset
+opkg install ipset
 opkg install ipt2socks 
-opkg install  iptables 
-opkg install  iptables-legacy 
-opkg install  iptables-mod-conntrack-extra 
+opkg install iptables 
+opkg install iptables-legacy 
+opkg install iptables-mod-conntrack-extra 
 opkg install iptables-mod-iprange 
-opkg install  iptables-mod-socket 
-opkg install  iptables-mod-tproxy 
-opkg install  kmod-ipt-nat 
-opkg install  dnsmasq-full
+opkg install iptables-mod-socket 
+opkg install iptables-mod-tproxy 
+opkg install kmod-ipt-nat 
+opkg install dnsmasq-full
 opkg install luci-app-passwall
 reboot
